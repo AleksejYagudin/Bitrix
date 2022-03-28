ccdce1315246ed208b7146fa0b9b79c0d5500e76
AleksejYagudin <49141634+AleksejYagudin@users.noreply.github.com>
2022-03-28 15:50:51 +0400

    add component users,list

---
+++ bitrix24.users.list/templates/.default/template.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+
+$APPLICATION->IncludeComponent(
+    'bitrix:main.ui.grid',
+    '',
+    $arResult['GRID_PARAMS'],
+    false
+);
